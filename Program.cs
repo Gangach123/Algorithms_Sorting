17d9ebc6c022ce783a1a877daf0cba41f03443ef
unknown <chilamakooru.gangaiiit@gmail.com>
2022-03-25 20:51:23 +0530

    New commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Algorithm_Problems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to algorithm problems");
+            Bubble_Sort bubbleSort = new Bubble_Sort();
+            bubbleSort.UserInput<string>();
+
+            Word_List wordList = new Word_List();
+            wordList.UserInput<string>();
+        }
+    }
+}
